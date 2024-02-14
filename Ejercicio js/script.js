c238fb49661b8c963d34ff7fe03549e68cb05aea
Loaxert <70897094+Santiago-Chavarro@users.noreply.github.com>
2024-02-13 23:09:52 -0500

    Add files via upload

---
+++ Ejercicio js/script.js
@@ -0,0 +1,21 @@
+function getdata(){
+    var nombre1a=document.getElementById('nombre1').value;
+    var nombre2a=document.getElementById('nombre2').value;
+    var nombre3a=document.getElementById('nombre3').value;
+    var nombre4a=document.getElementById('nombre4').value;
+    var apellido1a=document.getElementById('apellido1').value;
+    var apellido2a=document.getElementById('apellido2').value;
+    var apellido3a=document.getElementById('apellido3').value;
+    var apellido4a=document.getElementById('apellido4').value;
+    console.log(nombre1a);
+    console.log(nombre2a);
+    console.log(nombre3a);
+    console.log(nombre4a);
+    console.log(apellido1a);
+    console.log(apellido2a);
+    console.log(apellido3a);
+    console.log(apellido4a);
+
+    
+}
+
